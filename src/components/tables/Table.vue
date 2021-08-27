fccd7570f28465e8ed2184f127d1f3bb3aeff34f
liefwerk <natjageni@gmail.com>
2021-08-27 14:46:09 +0200

    Added actions

--- src/components/tables/Table.vue
+++ src/components/tables/Table.vue
@@ -40,7 +40,9 @@
               <td 
                 data-label="Action"
                 class="w-full lg:w-auto px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-2">Edit</a>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-2">View</a>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900 mr-2">Delete</a>
               </td>
             </tr>
           </tbody>
