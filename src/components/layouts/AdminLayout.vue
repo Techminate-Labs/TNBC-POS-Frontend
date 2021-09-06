0c687222f553bb2fdc1d940a51229d3f84abbac9
liefwerk <natjageni@gmail.com>
2021-09-06 17:16:18 +0200

    Added click outside sidebar to close it

--- src/components/layouts/AdminLayout.vue
+++ src/components/layouts/AdminLayout.vue
@@ -14,7 +14,7 @@
           @close-additional-sidebar="closeAdditionalSidebar"
           :class="openAdditionalSideBar ? 'active' : ''" 
         />
-        <div class="w-9/12 flex-grow overflow-x-hidden">
+        <div class="w-9/12 flex-grow overflow-x-hidden" @click="openAdditionalSideBar = false">
           <div class="bg-red-300 text-wite w-full py-2 px-8 text-lg" v-if="!isEmailVerified">
             <p>Your account has not been verified ! Please <button @click="requestEmailVerification" class="underline">send a verification email to your inbox.</button></p>
           </div>
